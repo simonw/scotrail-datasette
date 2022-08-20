448a56fdba019afe5add6f1854fdd0a5e5218578
Simon Willison <swillison@gmail.com>
2022-08-20 15:38:15 -0700

    Tweak column order

--- build-db.sh
+++ build-db.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 sqlite-utils insert scotrail.db announcements announcements.csv --csv --pk File
-sqlite-utils transform scotrail.db announcements --rename 'NRE ID' NRE_ID --rename 'mp3 file' 'mp3'
+sqlite-utils transform scotrail.db announcements \
+  --rename 'NRE ID' NRE_ID \
+  --rename 'mp3 file' 'mp3' \
+  -o File \
+  -o Transcription \
+  -o Category \
+  -o mp3 \
+  -o Notes \
+  -o Timestamp \
+  -o NRE_ID
 sqlite-utils scotrail.db "update announcements set mp3 = 'https://github.com/matteason/scotrail-announcements-june-2022/raw/main/announcements/' || mp3"
 
 # Enable search
