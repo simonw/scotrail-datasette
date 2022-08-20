ed92118283e23545c563a043143d92a9bc9d0380
Simon Willison <swillison@gmail.com>
2022-08-20 15:13:17 -0700

    Enable search, refs #2

--- build-db.sh
+++ build-db.sh
@@ -3,3 +3,5 @@ sqlite-utils insert scotrail.db announcements announcements.csv --csv --pk File
 sqlite-utils transform scotrail.db announcements --rename 'NRE ID' NRE_ID --rename 'mp3 file' 'mp3'
 sqlite-utils scotrail.db "update announcements set mp3 = 'https://github.com/matteason/scotrail-announcements-june-2022/raw/main/announcements/' || mp3"
 
+# Enable search
+sqlite-utils enable-fts scotrail.db announcements Transcription --tokenize porter
